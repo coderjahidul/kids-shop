c568a06c3b79128c7126781dba43a1f1c50c7602
coderjahidul <sobuz0349@gmail.com>
2026-05-21 10:21:44 +0600

    Cleanup full theme file

--- fix_php.py
+++
@@ -1,8 +0,0 @@
-for filename in ['header.php', 'footer.php', 'front-page.php']:
-    with open(filename, 'r', encoding='utf-8') as f:
-        content = f.read()
-    
-    content = content.replace('&lt;?php', '<?php').replace('?&gt;', '?>')
-    
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(content)
